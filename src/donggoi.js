02e33d0439009a1e939dee8165e3198d36b322cb
Hoang Trieu, Khang <khoangtrieu@paypal.com>
2020-12-03 23:40:54 -0800

    finish the parsing logic

--- src/donggoi.js
+++ src/donggoi.js
@@ -1,37 +1,42 @@
 const { readFileSync } = require('fs');
 const { parse } = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
-const { resolve } = require('path');
+const path = require('path');
 const { transform } = require('@babel/core');
 
 const modules = [];
 let id = 0;
-const entryFile = '../test/main.js';
+const entryFile = './main.js';
+const rootFolder = path.resolve(__dirname, '../test');
 
-const parseCode = file => {
-    const codeStr = readFileSync(file, 'utf8');
+const parseCode = (file, parentPath = '.') => {
+    const relativePath = file;
+    const absolutePath = `${path.resolve(
+        path.dirname(path.resolve(rootFolder, file)),
+        relativePath
+    )}`;
+
+    const codeStr = readFileSync(absolutePath, 'utf8');
     const astTree = parse(codeStr, { sourceType: 'module' });
 
     traverse(astTree, {
         ImportDeclaration: path => {
-            const relativePath = path.node.source.value;
-            const absolutePath = `${resolve(file, '..', relativePath)}.js`;
-
-            const module = {
-                relativePath,
-                absolutePath,
-                id: id++,
-                code: transform(codeStr, {
-                    presets: ['@babel/preset-env']
-                }).code
-            };
-
-            modules.push(module);
-            parseCode(absolutePath);
+            const relativePath = `${path.node.source.value}.js`;
+            parseCode(relativePath, file);
         }
     });
+
+    const module = {
+        relativePath,
+        absolutePath,
+        id: id++,
+        code: transform(codeStr, {
+            presets: ['@babel/preset-env']
+        }).code
+    };
+
+    modules.push(module);
 };
 
 parseCode(entryFile);
 console.log({ modules });
-debugger;
